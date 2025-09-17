3a4fa0844ea003f1c434f9117f4634d563e757fe
Derfies <Derfies@users.noreply.github.com>
2025-09-17 17:05:45 +1000

    [EDIT] - texture.py - Doing texture comparison by the wrapped value, not the instance itself.

--- editor/texture.py
+++ editor/texture.py
@@ -2,3 +2,8 @@ class Texture:
 
     def __init__(self, value):
         self.value = value
+
+    def __eq__(self, other):
+        if not isinstance(self, other.__class__):
+            return False
+        return self.value == other.value
